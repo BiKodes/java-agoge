73577e2ea321c7d91d476c940729a91140101afd
BikoCodes <bikocodes@gmail.com>
2023-08-02 06:17:43 +0300

    Chore: Getting user input

---
+++ thenewboston/gettingUserInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Matunda{
+    public static void main(String args[]){
+        Scanner maembe = new Scanner(System.in);
+        System.out.println(maembe.nextLine());
+        
+    }
+}
