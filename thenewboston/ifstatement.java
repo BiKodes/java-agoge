e7866a26bc48efbbc9e536d3ae6e6b12fbeb02b2
BikoCodes <bikocodes@gmail.com>
2023-08-13 15:45:12 +0300

    chore: java if conditional statement

---
+++ thenewboston/ifstatement.java
@@ -0,0 +1,11 @@
+class Wanyama{
+    public static void main(String args[]){
+        int paka = 6;
+
+        if (paka == 9){
+            System.out.println("Meow");
+        }else{
+            System.out.println("The cat can't meow!");
+        }
+    }
+}
